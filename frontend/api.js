9efb6c41625617db2777088a66cc3670bbced0d4
nicolinabl <nicolina.backlin@telia.com>
2026-02-25 14:51:45 +0100

    socket fixme comment

--- frontend/api.js
+++ frontend/api.js
@@ -1,2 +1,4 @@
 export const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8080";
 export const socketUrl = "http://localhost:8080"; 
+
+// FIXME right now all quests are added to questlist? use socket rooms to make sure user only see its own quests
