9ae4ea73886f4c15a1b9ba57963ad5b6592a9afb
nicolinabl <nicolina.backlin@telia.com>
2026-02-26 14:03:31 +0100

    zustand for global state management. Stor e for user and quests. Logic moved to stores for global access

--- frontend/src/stores/useUserStore.jsx
+++ frontend/src/stores/useUserStore.jsx
@@ -1,39 +1,41 @@
-import create from 'zustand'
-
-const storedUser = localStorage.getItem('user')
-const storedISLoggedIn = localStorage.getItem('isLoggedIn') === 'true'
+import { create } from 'zustand'
 
 export const useUserStore = create((set) => ({
-  // CREATE STATE = what is in the store (initial state):
-  user: '',
+  // CREATE STATE (what is in the store (initial state))
+  user: null,
   isLoggedIn: false,
-  // streak,
-  // todayQuestCompleted: false,
-  // moodUrl: ''
+ 
+  // SET STATE (change what is in the store)
 
-  // SET STATE = change what is in the store
-  // Log in a user
-  login: (userName) => {
-    localStorage.setItem('user', userName)
-    localStorage.setItem('isLoggedIn', 'true')
+  // Call getUser when app mounts to access user from local storage
+  getUser: () => {
+    const accessToken = localStorage.getItem('accessToken')
+    const userId = localStorage.getItem('userId')
+    const email = localStorage.getItem('userEmail')
+    // FIXME Look into name. Name data not accessed. How to get username when user logs in
+    const name = localStorage.getItem('userName')
 
-    set({
-      user: userName,
-      isLoggedIn: true,
-    })
+    // If both accessToken and useId exist in localStorage -> update store state. Keeps user logged in after page refresh
+    if (accessToken && userId) {
+      set({ user: {accessToken, userId, email, name}, isLoggedIn: true })
+    }
   },
 
-  // log out user
-  logout: () => {
-    // Clear localStorage
-    localStorage.removeItem('user')
-    localStorage.setItem('isLoggedIn', 'false')
+  login: (userData) => {
+    localStorage.setItem('accessToken', userData.accessToken)
+    localStorage.setItem('userId', userData.userId)
+    localStorage.setItem('userEmail', userData.email)
+    localStorage.setItem('userName', userData.userName)
+    set ({ user: userData, isLoggedIn: true })
+  },
 
-    // Update Zustand store
-    set({
-      user: '',
-      isLoggedIn: false,
-    })
+  logout: () => {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('userId')
+    localStorage.removeItem('userEmail')
+    localStorage.removeItem('userName')
+    set ({ user: null, isLoggedIn: false })
   },
 }))
+
 
