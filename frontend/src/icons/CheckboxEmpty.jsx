5616e01015af1112c083a60389d80d9500f91088
Julia Demianetc <julias@mac.home>
2026-03-05 00:46:42 +0100

    added checkbox components, icons and styles

---
+++ frontend/src/icons/CheckboxEmpty.jsx
@@ -0,0 +1,16 @@
+export const CheckboxEmpty = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill="#000000"
+        d="M4 2h16v2H4zm0 18h16v2H4zM2 4h2v16H2zm18 0h2v16h-2z"
+      />
+    </svg>
+  );
+};
