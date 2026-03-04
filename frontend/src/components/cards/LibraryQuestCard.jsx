b99394c86ffdee5920f77805ec2a89db4b4129a0
nicolinabl <nicolina.backlin@telia.com>
2026-03-04 13:10:58 +0100

    hard coded library list + styling

---
+++ frontend/src/components/cards/LibraryQuestCard.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const LibraryQuestCard = (message, timeNeeded, category) => {
+  return (
+    <Container>
+      <Div>
+        <div>
+          <P>{message}</P>
+          {/* <p>Category: {category}</p> */}
+          <TimeP>{timeNeeded} min</TimeP>
+        </div>
+      </Div>
+      <button>Add to my quests</button>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  padding: 5px 16px;
+  justify-content: space-between;
+  border-radius: 12px;
+  border: 1px solid var(--accent-color);
+  background-color: #FFFFFF;
+  box-shadow: 0 2px 2px 0 #DBDBDB;
+  margin: 5px 10px;
+
+`
+
+const P = styled.p`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin: 0;
+`
+
+const TimeP = styled.p`
+  font-family: Roboto;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: var(--accent-color);
+  margin: 0;
+`
+
+const Div = styled.div`
+  display: flex;
+  gap: 5px;
+`
