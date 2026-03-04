b6e6eeac8453c39df23cf59325d5eed905308d0e
Julia Demianetc <julias@mac.home>
2026-03-04 22:35:36 +0100

    switched to browserrouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
