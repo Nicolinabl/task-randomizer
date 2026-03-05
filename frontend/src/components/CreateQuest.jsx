d59667b2d8e225a6dbe3fbd53a8b322bb2e6f95f
Julia Demianetc <julias@apples-MacBook-Pro.local>
2026-03-05 11:59:55 +0100

    fixing navbar conflict

--- frontend/src/components/CreateQuest.jsx
+++ frontend/src/components/CreateQuest.jsx
@@ -1,57 +1,56 @@
-import styled from 'styled-components'
-import { useState } from 'react'
+import styled from "styled-components";
+import { useState } from "react";
 // import { apiUrl } from '../../api'
-import { useQuestStore } from '../stores/useQuestStore'
+import { useQuestStore } from "../stores/useQuestStore";
 
 // component for creating quest
 export const CreateQuest = () => {
   // state variables for form inputs
-  const [message, setMessage] = useState('')
-  const [timeNeeded, setTimeNeeded] = useState('')
-  const [category, setCategory] = useState('')
-  const [error, setError] = useState(null)
+  const [message, setMessage] = useState("");
+  const [timeNeeded, setTimeNeeded] = useState("");
+  const [category, setCategory] = useState("");
+  const [error, setError] = useState(null);
 
-  const createQuest = useQuestStore((state) => state.createQuest)
+  const createQuest = useQuestStore((state) => state.createQuest);
 
   // Handle form submission
-  const handleSubmit =  async (event) => {
-    event.preventDefault()
-    setError(null)
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError(null);
 
     if (!message.trim()) {
-      setError('You need to add a quest')
-      return
+      setError("You need to add a quest");
+      return;
     }
 
     if (!timeNeeded) {
-      setError('You need to add time needed')
+      setError("You need to add time needed");
     }
 
-    const result = await createQuest(message, timeNeeded, category)
+    const result = await createQuest(message, timeNeeded, category);
 
     if (!result.success) {
-      setError(result.error)
-      return
+      setError(result.error);
+      return;
     }
 
     // After form i submitted, clear the input field
-    setMessage('')
-    setTimeNeeded('')
-    setCategory('')
-
-}
+    setMessage("");
+    setTimeNeeded("");
+    setCategory("");
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
       <h2>Create new quest</h2>
       <Label>
         What do you need to do?
-        <Input 
-          type="text" 
-          placeholder='Quest' 
-          value={message} 
-          onChange={(event) => setMessage(event.target.value)}/>
-        
+        <Input
+          type="text"
+          placeholder="Quest"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
         {/* <select 
           name="category" 
           value={category}
@@ -64,25 +63,22 @@ export const CreateQuest = () => {
           <option value="personal">Personal</option>
         </select> */}
         {/* NOTE: category commented out for now. Not used currently */}
-
         <label>
           How many minutes will it take you?
-          <Input 
-            name="time" 
-            type='number'
-            placeholder='Time needed' 
-            value={timeNeeded} 
-            onChange={(event) => setTimeNeeded(event.target.value)} 
+          <Input
+            name="time"
+            type="number"
+            placeholder="Time needed"
+            value={timeNeeded}
+            onChange={(event) => setTimeNeeded(event.target.value)}
           />
         </label>
-
         {error && <p>{error}</p>}
-
         <Button type="submit">Add new quest</Button>
       </Label>
     </Form>
-  )
-}
+  );
+};
 
 const Form = styled.form`
   margin: 10px;
@@ -96,16 +92,16 @@ const Form = styled.form`
 const Label = styled.label`
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Input = styled.input`
   border: none;
   border-radius: 12px;
   padding: 15px 16px 14px 16px;
-  background: #F4F0FF;
+  background: #f4f0ff;
   margin: 16px 0;
   width: 100%;
-`
+`;
 
 const Button = styled.button`
   display: flex;
@@ -116,10 +112,10 @@ const Button = styled.button`
   align-items: center;
   gap: 10px;
   border-radius: 12px;
-  border: 1px solid #1D30CE;
-  background: #866DEB;
-  box-shadow: 2px 4px 4px 0 rgba(139, 139, 139, 0.30);
+  border: 1px solid #1d30ce;
+  background: #866deb;
+  box-shadow: 2px 4px 4px 0 rgba(139, 139, 139, 0.3);
   color: white;
-`
+`;
 
 // TODO: Add authentication check - redirect to login if no accessToken
