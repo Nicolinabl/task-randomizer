67b037a073ed6b5c3f15bcf484af014c591a9b84
Julia Demianetc <julias@apples-MacBook-Pro.local>
2026-03-05 12:14:58 +0100

    -

--- frontend/src/pages/RewardPage.jsx
+++ frontend/src/pages/RewardPage.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import { useEffect, useState, useMemo } from "react";
+
 export const Rewards = () => {
-  return <h1>Rewards page</h1>;
+  return (
+    <div>
+      <button>X</button>
+      <h1>Here's your daily dose of smile</h1>
+      <div>
+        Reward meme from api
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 };
+
+//Redirect user here from Quest of the day page, after user checked quest as done
+//Show a random meme of the day
+//Maybe add a funny api with explosion emoji and "You were amazing today" before closing this page (with the button for user)
 
 // TODO: Put content here + set up route
